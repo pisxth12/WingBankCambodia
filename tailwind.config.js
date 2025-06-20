9c86eba05ac3b34c642616083199281af74dd106
pisxth12 <pxsxth1214@gmail.com>
2025-06-20 17:46:15 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,23 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'max320': { max: '320px' },
-        'max321': { max: '321px' },
+        'screen320':  '320px',
+        'screen321':  '321px',
 
-        'max300': { max: '300px' },
-        'max350': { max: '350px' },
-        'max400': { max: '400px' },
+        'screen300':  '300px',
+        'screen350':  '350px',
+        'screen400':  '400px',
+        'screen575':  '575px',
+        'screen767' : '767px',
+        'screen1025' : '1025px',
+        'min1025' : {min : '1025px'},
+        'max1024' : {max : '1024px'},
+
+        
+
+
+
+
       },
       colors: {
         wing: {
@@ -19,9 +30,34 @@ module.exports = {
         },
       },
       fontSize: {
-        wing: {
-          navFont_1rem: '1rem',
-        },
+        'wing-nav': '1rem',
+        'wing-title-lg': '56px',
+        'wing-title-md': '50px',
+        'wing-title-sm': '35px',
+
+
+      },
+      borderWidth: {
+        'wingBorder1': '1px',
+        'wingBorder2': '2px',
+        'wingBorder3': '3px',
+        'wingBorder4': '4px',
+        'wingBorder5': '5px',
+
+
+        'custom': '7px'
+      },
+       fontWeight: {
+        'wingUltraSmall2x': '10',
+
+        'wingUltraSmall': '50',
+        'wingsmall': '100',
+        'wingMedium': '200',
+        'wingLight': '300',
+        'wingNormal': '400',
+        'wingMedium': '500',
+        'wingSemiBold': '600',
+        'wingBold': '700',
       },
     },
   },
